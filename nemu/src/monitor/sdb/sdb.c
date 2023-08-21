2dc7776cd04c1f5a222b5e5233ca6d1468fdf1c1
tracer-ysyx <tracer@ysyx.org>
2023-08-21 23:27:18 +0800

    > compile NEMU ysyx_22040000 闫勇旭 Linux yanxy-G3-3579 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:27:17 up 2 days, 10:53,  1 user,  load average: 0.82, 0.93, 0.84

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -18,6 +18,8 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "sdb.h"
+#include <memory/paddr.h>
+
 //是否处于批处理模式
 static int is_batch_mode = false;
 
@@ -63,7 +65,7 @@ static int cmd_si(char *args);
 
 static int cmd_info(char *args);
 
-// static int cmd_x(char *args);
+static int cmd_x(char *args);
 
 // static int cmd_p(char *args);
 
@@ -81,7 +83,7 @@ static struct {
   { "q", "Exit NEMU", cmd_q },
   { "si","Exec step by step",cmd_si},
   {"info","Print the information of registers or watchpoints",cmd_info},
-  // {"x","Scan the memory",cmd_x},
+  {"x","Scan the memory",cmd_x},
   // {"p","Calculate the value of the expression",cmd_p},
   // {"w","Set a watchpoint",cmd_w},
   // {"d","Delete a watchpoint",cmd_d},
@@ -146,27 +148,27 @@ static int cmd_info(char *args){
   return 0;
 }
 
-// static int cmd_x(char *args)
-// {
-//   char *arg = strtok(NULL," ");
-//   if(arg == NULL){
-//     printf("Please input the argument!\n");
-//     return 0;
-//   }
-//   int n;
-//   paddr_t addr;
-//   sscanf(arg,"%d",&n);
-//   arg = strtok(NULL," ");
-//   if(arg == NULL){
-//     printf("Please input the argument!\n");
-//     return 0;
-//   }
-//   sscanf(arg,"%x",&addr);
-//   for(int i = 0;i < n;i++){
-//     printf("0x%08x: 0x%08x\n",addr + i * 4,paddr_read(addr + i * 4,4));
-//   }
-//   return 0;
-// }
+static int cmd_x(char *args)
+{
+  char *arg = strtok(NULL," ");
+  if(arg == NULL){
+    printf("Please input the argument!\n");
+    return 0;
+  }
+  int n;
+  paddr_t addr;
+  sscanf(arg,"%d",&n);
+  arg = strtok(NULL," ");
+  if(arg == NULL){
+    printf("Please input the argument!\n");
+    return 0;
+  }
+  sscanf(arg,"%x",&addr);
+  for(int i = 0;i < n;i++){
+    printf("0x%08x: 0x%08x\n",addr + i * 4,paddr_read(addr + i * 4,4));
+  }
+  return 0;
+}
 
 void sdb_set_batch_mode() {
   is_batch_mode = true;
