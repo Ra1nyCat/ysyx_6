8d63646ba637297c4f2f47c1ac63b359628876b6
tracer-ysyx <tracer@ysyx.org>
2023-08-23 10:10:14 +0800

    > compile NEMU ysyx_22040000 闫勇旭 Linux yanxy-G3-3579 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:10:13 up 3 days, 21:36,  1 user,  load average: 1.13, 0.98, 0.85

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -235,10 +235,9 @@ word_t eval(int l,int r,bool *success)
       case '-': return val1 - val2;
       case '*': return val1 * val2;
       case '/': return val1 / val2;
-      default: return -1;
+      default: assert(0);
     }
   }else{
-    printf("Error: Expr is not legal!\n");
     *success=false;
     return -1;
   }
