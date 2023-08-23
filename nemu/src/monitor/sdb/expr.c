b878fdb799a44b995f1047a633cd93ea97f70e46
tracer-ysyx <tracer@ysyx.org>
2023-08-23 23:05:35 +0800

    > run NEMU ysyx_22040000 闫勇旭 Linux yanxy-G3-3579 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:05:35 up 4 days, 10:31,  1 user,  load average: 0.75, 0.46, 0.48

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -130,19 +130,19 @@ static bool make_token(char *e) {
           case TK_NUMBER: 
             tokens[nr_token].type = rules[i].token_type;
             memset(tokens[nr_token].str,0,sizeof(tokens[nr_token].str));
-            strncpy(tokens[nr_token].str,substr_start,substr_len-1);
+            strncpy(tokens[nr_token].str,substr_start,substr_len);
             nr_token++;
             break;
           case TK_XNUMBER:
             tokens[nr_token].type = rules[i].token_type;
             memset(tokens[nr_token].str,0,sizeof(tokens[nr_token].str));
-            strncpy(tokens[nr_token].str,substr_start,substr_len-1);
+            strncpy(tokens[nr_token].str,substr_start,substr_len);
             nr_token++;
             break;
           case TK_REGISTER:
             tokens[nr_token].type = rules[i].token_type;
             memset(tokens[nr_token].str,0,sizeof(tokens[nr_token].str));
-            strncpy(tokens[nr_token].str,substr_start+1,substr_len-1);
+            strncpy(tokens[nr_token].str,substr_start+1,substr_len);
             nr_token++;
             break;
           case TK_AND:
