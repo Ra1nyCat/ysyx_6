fd599538fab71089a89b1ffc57f93ea8713099fb
tracer-ysyx <tracer@ysyx.org>
2023-08-24 00:06:05 +0800

    > compile NEMU ysyx_22040000 闫勇旭 Linux yanxy-G3-3579 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:06:05 up 4 days, 11:32,  1 user,  load average: 0.23, 0.14, 0.20

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -43,7 +43,7 @@ static struct rule {
   {"\\/", '/'},         // divide
   {"\\(", '('},         // left bracket
   {"\\)", ')'},         // right bracket
-  {"[0-9]+", TK_NUMBER},// 10-number
+  {"[1-9][0-9]*", TK_NUMBER},// 10-number
   {"0[xX][0-9a-fA-F]+",TK_XNUMBER},// 16-number
   {"$[a-zA-Z]+[0-9]+",TK_REGISTER},// register
   {"!=",TK_NONEQUAL},// not equal
