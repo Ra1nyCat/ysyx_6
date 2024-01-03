836b711e588cfa712b55c88bc260594192fb3e3a
tracer-ysyx <tracer@ysyx.org>
2024-01-03 23:28:33 +0800

    > compile NEMU ysyx_22040000 闫勇旭 Linux yanxy-G3-3579 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:28:33 up 4 days,  1:33,  1 user,  load average: 0.88, 0.91, 0.89

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -133,7 +133,7 @@ static int parse_args(int argc, char *argv[]) {
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
-      case 'e': load_elf_symbols(optarg); break;
+      case 'e': printf("load file:%s\n",optarg);load_elf_symbols(optarg); break;
       case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
