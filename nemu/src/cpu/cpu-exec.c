033c24d5fe45b6612bbc37d4f76fd1b0f85976dc
tracer-ysyx <tracer@ysyx.org>
2024-01-03 23:54:01 +0800

    > compile NEMU ysyx_22040000 闫勇旭 Linux yanxy-G3-3579 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:54:01 up 4 days,  1:59,  1 user,  load average: 1.09, 1.14, 1.08

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -98,7 +98,7 @@ int find_func(Elf32_Addr value)
   for(int i=0;i<symtab_size;i++){
     // printf("L:%x  R:%x  value:%x\n",symtab[i].st_value,symtab[i].st_value+symtab[i].st_size-1,value);
     if(ELF32_ST_TYPE(symtab[i].st_info)!=STT_FUNC)continue;
-    if(symtab[i].st_value<=value||symtab[i].st_value+symtab[i].st_size-1>=value){
+    if(symtab[i].st_value==value){
       return i;
     }
   }
